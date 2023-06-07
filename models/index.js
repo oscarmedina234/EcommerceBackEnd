8e93ba8369a9031d634066214b21bee81d9f1f16
oscarmedina234 <oscarmedina234@gmail.com>
2023-06-07 12:05:10 -0500

    changed model index

--- models/index.js
+++ models/index.js
@@ -5,8 +5,9 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.hasOne(Category, {
-  foreignKey: 'category_id',
+
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
 });
 
 // Categories have many Products
